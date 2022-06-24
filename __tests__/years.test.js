0adb8268d70b9cb923c50f19a4fdd806a0874055
Liam Cammpbell <liam22campbell@gmail.com>
2022-06-24 08:33:57 -0700

    First test red.

--- __tests__/years.test.js
+++ __tests__/years.test.js
@@ -1,10 +1,10 @@
-import exampleTestFunction from '../src/js/years.js';
+import EarthYears from '../src/js/years.js';
 
 
-describe("exampleTestFunction", () => {
-
-  test("should....", () => {
-    expect(exampleTestFunction()).toEqual("hi there, from the deck")
+describe("EarthYears", () => {
+  test("Should set the variable age equal to a number", () => {
+    const ageOnEarth = new EarthYears(3);
+    expect(ageOnEarth.earthYears).toEqual(3)
   });
 
 });
