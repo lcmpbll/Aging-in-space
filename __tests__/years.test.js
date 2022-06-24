2ea61c9889fc37d44653390138e7a416ecfa029d
Liam Cammpbell <liam22campbell@gmail.com>
2022-06-24 08:37:42 -0700

    Alter first test.

--- __tests__/years.test.js
+++ __tests__/years.test.js
@@ -3,8 +3,8 @@ import EarthYears from '../src/js/years.js';
 
 describe("EarthYears", () => {
   test("Should set the variable age equal to a number", () => {
-    const ageOnEarth = new EarthYears(3);
-    expect(ageOnEarth.earthYears).toEqual(3)
+    const age = new EarthYears(3);
+    expect(age.earthYears).toEqual(3)
   });
 
 });
