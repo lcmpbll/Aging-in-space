a3713b02a004b3a54cc110fdb8b8e8f7528dc240
Liam Cammpbell <liam22campbell@gmail.com>
2022-06-24 09:40:47 -0700

    fail fourth test.

--- __tests__/years.test.js
+++ __tests__/years.test.js
@@ -17,3 +17,8 @@ test('should correctly return the inputted Earth age in Venus years', () => {
   const age = new EarthYears(3);
   expect (age.venusAge()).toEqual("4.80");
 });
+
+test('should correctly return the inputted Earth age in Mars years ', () => {
+  const age = new EarthYears(3);
+  expect(age.marsAge()).toEqual("5.64");
+});
