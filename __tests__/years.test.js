7fc64729a994de4580540ede0d06d553102f13b0
Liam Cammpbell <liam22campbell@gmail.com>
2022-06-24 09:03:30 -0700

    Fail second test.

--- __tests__/years.test.js
+++ __tests__/years.test.js
@@ -7,3 +7,8 @@ describe("EarthYears", () => {
     expect(age.ageOnEarth).toEqual(3)
   });
 });
+
+test('should correctly return the inputted earth age in mercury years', () => {
+  const age = new EarthYears(3);
+  expect(age.ageOnMercury()).toEqual("12.4 years old on the planet Mercury");
+});
