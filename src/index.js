51f9147796b4cec8da2ec9aa35fdb4d8f155e857
Liam Cammpbell <liam22campbell@gmail.com>
2022-06-24 16:52:17 -0700

    Clean up unused sections in index.html and index.js.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import EarthYears from './js/years.js';
-import $ from 'jquery';
+// import $ from 'jquery';
 // import 'bootstrap';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import './css/styles.css';
 
 
-  $("form#example-form").submit(function(event) {
-    event.preventDefault();
-    const numbers = $("#number").val();
-    const text = $("#text").val();
-    const exampleText = exampleTestFunction(numbers, text);
-    console.log(exampleText);
-    $("#output").text(exampleText);
-  
-    function logReset() {
-    }
-  });
-});
+
